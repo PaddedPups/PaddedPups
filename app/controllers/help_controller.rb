7cd1e236c95522b4d34e753ff786488649ee6f42
Donovan Daniels <hewwo@yiff.rocks>
2024-06-22 00:44:59 -0500

    Render redirected contents in help pages

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -16,6 +16,9 @@ class HelpController < ApplicationController
       @help = HelpPage.find_by!(name: params[:id])
     end
     authorize(@help)
+    if @help.wiki.parent.present?
+      @wiki_redirect = WikiPage.find_by(title: @help.wiki.parent_name)
+    end
     respond_with(@help)
   end
 
