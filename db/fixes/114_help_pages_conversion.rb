c4d6f591f48b83bf397fc9a2367b0eb34eb4a2ca
Donovan Daniels <hewwo@yiff.rocks>
2024-06-27 01:39:40 -0500

    fix fixer

--- db/fixes/114_help_pages_conversion.rb
+++ db/fixes/114_help_pages_conversion.rb
@@ -3,10 +3,12 @@
 
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "config", "environment"))
 
-HelpPage.find_each do |help|
-  help.update!(wiki_page_name: help.wiki_page_in_database)
-end
+ModAction.without_logging do
+  HelpPage.find_each do |help|
+    help.update!(wiki_page_name: help.wiki_page_in_database)
+  end
 
-ModAction.where(action: %i[help_create help_update help_delete]).find_each do |mod|
-  mod.update!(values: { name: mod.name, wiki_page_title: mod.wiki_page, wiki_page_id: WikiPage.titled(mod.wiki_page)&.id })
+  ModAction.where(action: %i[help_create help_update help_delete]).find_each do |mod|
+    mod.update!(values: { name: mod.name, wiki_page_title: mod.wiki_page, wiki_page_id: WikiPage.titled(mod.wiki_page)&.id })
+  end
 end
