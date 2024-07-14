243f6ccf0b51682d28a7ffb3070c51cf0b5ab97b
Donovan Daniels <hewwo@yiff.rocks>
2024-07-14 13:10:03 -0500

    update csp

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,9 +9,9 @@
 Rails.application.configure do
   config.content_security_policy do |policy|
     policy.default_src(:self)
-    policy.script_src(:self, "js-agent.newrelic.com", "https://www.google.com/recaptcha/", "https://www.gstatic.com/recaptcha/", "https://www.recaptcha.net/", "https://cdnjs.cloudflare.com")
+    policy.script_src(:self, "https://www.google.com/recaptcha/", "https://www.gstatic.com/recaptcha/", "https://www.recaptcha.net/", "https://cdnjs.cloudflare.com", "https://plausible.furry.computer")
     policy.style_src(:self, :unsafe_inline, "https://cdnjs.cloudflare.com")
-    policy.connect_src(:self)
+    policy.connect_src(:self, "https://plausible.furry.computer")
     policy.object_src(:self, "static.femboy.fan")
     policy.media_src(:self, "static.femboy.fan")
     policy.frame_ancestors(:none)
